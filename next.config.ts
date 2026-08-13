f31455978a202d6a7da98694dd4952d17fe5f33b
ljylhm <948021695@qq.com>
2026-08-13 17:19:15 +0800

    fix: rely on automatic ffmpeg output tracing

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["ffmpeg-static"],
-  outputFileTracingIncludes: {
-    "/api/bilibili/mp3": ["./node_modules/ffmpeg-static/ffmpeg"],
-  },
 };
 
 export default nextConfig;
